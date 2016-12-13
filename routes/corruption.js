e84f29b81f889d31c8a3ddb2e7bd85ab06537132
ameliagoodman <ameliago@sas.upenn.edu>
2016-12-13 13:42:05 -0500

    working on fixing meriah's code to query for the year but currently broken

--- routes/corruption.js
+++ routes/corruption.js
@@ -5,7 +5,8 @@ var connection = mysql.createConnection({
   host     : 'cis550project.cgregrpimppx.us-west-2.rds.amazonaws.com',
   user     : 'welovedope',
   password : 'dope420420',
-  database : 'cis550project'
+  database : 'cis550project',
+  multipleStatements: true
 });
 AWS.config.loadFromPath('./config.json');
 
@@ -14,220 +15,216 @@ if (!AWS.config.credentials || !AWS.config.credentials.accessKeyId)
 
 var db = new AWS.DynamoDB.DocumentClient();
 
-var params = {
-    TableName : "Corruption"
+var paramsAll = {
+    TableName : "Corruption",
 };
 
-var corruptionOverall;
-var yearOne;
-var yearTwo;
-var yearThree;
-var yearFour;
-var yearFive;
-var yearSix;
+var params1995 = {
+    TableName : "Corruption",
+    KeyConditionExpression: "#yr",
+    ExpressionAttributeNames:{
+        "#yr": "ranking[:yyyy]"
+    },
+    ExpressionAttributeValues: {
+        ":yyyy":1995
+    }
+};
+var params1996 = {
+    TableName : "Corruption",
+    KeyConditionExpression: "#year = :1996"
+};
+var params2000 = {
+    TableName : "Corruption",
+    KeyConditionExpression: "#year = :2000"
+};
+var params2004 = {
+    TableName : "Corruption",
+    KeyConditionExpression: "#year = :2004"
+};
+var params2008 = {
+    TableName : "Corruption",
+    KeyConditionExpression: "#year = :2008"
+};
+var params2015 = {
+    TableName : "Corruption",
+    KeyConditionExpression: "#year = :2015"
+};
 
-function query_db(res) {
-	db.scan(params, function(err, data) {
+
+var corruptionOverall;
+var year1995;
+var year1996;
+var year2000;
+var year2004;
+var year2008;
+var year2015;
+
+function sortMe(a, b) {
+  var x = a.country;
+  var y = b.country;
+  if (x < y) {
+    return -1;
+  }
+  if (x > y) {
+    return 1;
+  }
+  return 0;
+}
+
+function query_all() {
+	db.scan(paramsAll, function(err, data) {
       if (err){
         console.log(err, err.stack);
       }
       else {
         data.Items.sort(function (a,b) {
-          var x = a.country;
-          var y = b.country;
-          if (x < y) {
-            return -1;
-          }
-          if (x > y) {
-            return 1; 
-          }
-          return 0;
+          return sortMe(a,b);
         });
-        query_1(res, data.Items);
+        corruptionOverall = data.Items;
       }
     });
 }
 
-function query_1(res, items1) {
-  db.scan(params, function(err, data) {
+function query_1995() {
+  db.scan(params1995, function(err, data) {
     if (err){
-        console.log(err, err.stack);
-      }
-      else {
-        data.Items.sort(function (a,b) {
-          var x = a.ranking[1995];
-          var y = b.ranking[1995];
-          if (x < y) {
-            return -1;
-          }
-          if (x > y) {
-            return 1; 
-          }
-          return 0;
-        });
-        query_2(res, items1, data.Items);
-    }
-  });
-}
-
-function query_2(res, items1, items2) {
-  db.scan(params, function(err, data) {
-    if (err){
-        console.log(err, err.stack);
-      }
-      else {
-        data.Items.sort(function (a,b) {
-          var x = a.ranking[1996];
-          var y = b.ranking[1996];
-          if (x < y) {
-            return -1;
-          }
-          if (x > y) {
-            return 1; 
-          }
-          return 0;
-        });
-        query_3(res, items1, items2, data.Items);
-    }
-  });
-}
-
-function query_3(res, items1, items2, items3) {
-  db.scan(params, function(err, data) {
-    if (err){
-        console.log(err, err.stack);
-      }
-    else {
-      data.Items.sort(function (a,b) {
-        var x = a.ranking[2000];
-        var y = b.ranking[2000];
-        if (x < y) {
-          return -1;
-        }
-        if (x > y) {
-          return 1; 
-        }
-        return 0;
-      });
-      yearThree = data.Items;
-      query_4(res, items1, items2, items3, data.Items);
-    }
-  });
-}
-
-function query_4(res, items1, items2, items3, items4) {
-  db.scan(params, function(err, data) {
-    if (err){
-        console.log(err, err.stack);
-      }
-    else {
-      data.Items.sort(function (a,b) {
-        var x = a.ranking[2004];
-        var y = b.ranking[2004];
-        if (x < y) {
-          return -1;
-        }
-        if (x > y) {
-          return 1; 
-        }
-        return 0;
-      });
-      yearFour = data.Items;
-      query_5(res, items1, items2, items3, items4, data.Items);
-    }
-  });
-}
-
-function query_5(res, items1, items2, items3, items4, items5) {
-  db.scan(params, function(err, data) {
-    if (err){
-        console.log(err, err.stack);
+      console.log(err, err.stack);
     }
     else {
-      data.Items.sort(function (a,b) {
-        var x = a.ranking[2008];
-        var y = b.ranking[2008];
-        if (x < y) {
-          return -1;
-        }
-        if (x > y) {
-          return 1; 
-        }
-        return 0;
-      });
-      yearFive = data.Items;
-      query_6(res, items1, items2, items3, items4, items5, data.Items);
-    }
-  });
-}
-
-function query_6(res, items1, items2, items3, items4, items5, items6) {
-  db.scan(params, function(err, data) {
-    if (err){
-        console.log(err, err.stack);
-      }
-      else {
-        data.Items.sort(function (a,b) {
-          var x = a.ranking[2015];
-          var y = b.ranking[2015];
-          if (x < y) {
-            return -1;
-          }
-          if (x > y) {
-            return 1; 
-          }
-          return 0;
-        });
-       res.render('corruption.jade', {title: "Corruption Data", 
-          "results":  items1, 
-          "yearOne": items2,
-          "yearTwo": items3,
-          "yearThree": items4,
-          "yearFour": items5,
-          "yearFive": items6,
-          "yearSix": data.Items
-        });
-    }
-    
-  });
-      
-}
-function query_sql(res, items) {
-  var dope = { country: "USA", 
-    1994: { 
-      ranking: "2",
-      doping: "5"
-    }
-  };
-  for (var key in items) {
-    console.log("\n");
-    for (var y in items[key].ranking) {
-      console.log("COUNTRY" + items[key].country + "YEAR" + y);
-      query = "SELECT origin_country FROM doping_athletes WHERE origin_country = " + items[key].country;
-      connection.query(query, function(err, rows, fields) {
-        if (rows) {
-          console.log("COUNTRY :" + rows[0]);
-        }
-      })
-      // query = "SELECT COUNT(*) as c FROM doping_athletes WHERE origin_country = " + items[key].country + " AND year = " + y;
-      // connection.query(query, function(err, rows, fields) {
-      //   if (err) console.log(err);
-      //   else {
-      //     var dope_score = null;
-      //     if(rows[0] != null) {
-      //       dope_score = rows[0].c;
-      //     } else {
-      //       dope_score = 0;
-      //     }
-      //     dope.push({country: items[key].country, y: {ranking: items[key].ranking[y], doping: dope_score}});
-      //   }
+      console.log("Query succeeded.");
+      data.Items.forEach(function(item) {
+          console.log(" -", item.year + ": " + item);
       // });
-    }  
+      // data.Items.sort(function (a,b) {
+      //   return sortMe(a,b);
+      // });
+      // year1995 = data.Items;
+      });
+    }
+  });
+}
+
+function query_1996() {
+  db.scan(params1996, function(err, data) {
+    if (err){
+        console.log(err, err.stack);
+      }
+      else {
+        data.Items.sort(function (a,b) {
+          return sortMe(a, b);
+        });
+        year1996 = data.Items;
+    }
+  });
+}
+
+function query_2000() {
+  db.scan(params2000, function(err, data) {
+    if (err){
+        console.log(err, err.stack);
+      }
+    else {
+      data.Items.sort(function (a,b) {
+        return sortMe(a,b);
+      });
+      year2000 = data.Items;
+    }
+  });
+}
+
+function query_2004() {
+  db.scan(params2004, function(err, data) {
+    if (err){
+        console.log(err, err.stack);
+      }
+    else {
+      data.Items.sort(function (a,b) {
+        return sortMe(a,b);
+      });
+      year2004 = data.Items;
+    }
+  });
+}
+
+function query_2008() {
+  db.scan(params2008, function(err, data) {
+    if (err){
+        console.log(err, err.stack);
+    }
+    else {
+      data.Items.sort(function (a,b) {
+        return sortMe(a,b);
+      });
+      year2008 = data.Items;
+    }
+  });
+}
+
+function query_2015() {
+  db.scan(params2015, function(err, data) {
+    if (err){
+        console.log(err, err.stack);
+    }
+    else {
+      data.Items.sort(function (a,b) {
+        return sortMe(a,b);
+      });
+      year2015 = data.Items;
+    }
+  });
+}
+
+function showData(res) {
+  res.render('corruption.jade',
+    {title: "Corruption Data",
+    "results":  corruptionOverall,
+    "year1995": year1995,
+    "year1996": year1996,
+    "year2000": year2000,
+    "year2004": year2004,
+    "year2008": year2008,
+    "year2015": year2015
+    }
+  );
+}
+
+
+function query_sql(res, items) {
+  var query2009 = "SELECT count(*) FROM doping_athletes WHERE year>2008 AND origin_country = ";
+  var query2005 = "SELECT count(*) FROM doping_athletes WHERE year>2004 AND year<2009 AND origin_country = ";
+  var query2001 = "SELECT count(*) FROM doping_athletes WHERE year>2000 AND year<2005 AND origin_country = ";
+  var query1997 = "SELECT count(*) FROM doping_athletes WHERE year>1995 AND year<2001 AND origin_country = ";
+  var query1995 = "SELECT count(*) FROM doping_athletes WHERE year>1990 AND year<1996 AND origin_country = ";
+  var allQueries2009 = "";
+  var allQueries2005 = "";
+  var allQueries2001 = "";
+  var allQueries1997 = "";
+  var allQueries1995 = "";
+
+  for (var key in corruptionOverall) {
+    allQueries2009 += query2009 + corruptionOverall[key].country + "; ";
+    allQueries2005 += query2005 + corruptionOverall[key].country + "; ";
+    allQueries2001 += query2001 + corruptionOverall[key].country + "; ";
+    allQueries1997 += query1997 + corruptionOverall[key].country + "; ";
+    allQueries1995 += query1995 + corruptionOverall[key].country + "; ";
   }
+  connection.query(allQueries2009 + allQueries2005 + allQueries2001 + allQueries1997 + allQueries1995, function(err, rows, fields) {
+    if (err) console.log(err);
+    else {
+      console.log(rows);
+    }
+  });
 }
 
 /////
 // This is what's called by the main app
 exports.do_work = function(req, res){
-	query_db(res);
+	// query_all();
+  query_1995();
+  // query_1996();
+  // query_2000();
+  // query_2004();
+  // query_2008();
+  // query_2015();
+
 };
